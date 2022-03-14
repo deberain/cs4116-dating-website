35d4362b73080f9891d240f8e69eff0c20125e60
Jonathan Falvey <JohnnyFalvey01@hotmail.com>
2022-03-14 20:02:41 +0000

    fixed small error with nav links

--- chats.php
+++ chats.php
@@ -44,13 +44,13 @@ if (isset($_POST['logout'])) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="browse.php">Browse </a>
                     </li>
                     <li class="nav-item active">
-                        <a class="nav-link" href="chats.php">Chats </a>
+                        <a class="nav-link" href="#">Chats </a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ml-auto">
