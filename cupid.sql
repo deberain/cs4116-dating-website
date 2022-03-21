f037c0945e261d0072827e59b2c2d0d96bfe6562
Jonathan Falvey <55098346+deberain@users.noreply.github.com>
2022-03-21 19:38:12 +0000

    Uploading DB SQL file

---
+++ cupid.sql
@@ -0,0 +1,333 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 09, 2022 at 08:30 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cupid`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interactions`
+--
+
+CREATE TABLE `interactions` (
+  `interaction_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `target_user` int(11) NOT NULL,
+  `interaction_type` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interaction_types`
+--
+
+CREATE TABLE `interaction_types` (
+  `interaction_type_id` int(11) NOT NULL,
+  `interaction_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interests`
+--
+
+CREATE TABLE `interests` (
+  `interest_id` int(11) NOT NULL,
+  `interest_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `interests`
+--
+
+INSERT INTO `interests` (`interest_id`, `interest_name`) VALUES
+(1, 'Sport'),
+(2, 'Music'),
+(3, 'TV'),
+(4, 'Movies'),
+(5, 'Dancing'),
+(6, 'Video Games'),
+(7, 'Cooking'),
+(8, 'Photography'),
+(9, 'Animals'),
+(10, 'Anime'),
+(11, 'Fashion'),
+(12, 'Science'),
+(13, 'Puzzles'),
+(14, 'Board Games'),
+(15, 'Art');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `match_id` int(11) NOT NULL,
+  `user_one_id` int(11) NOT NULL,
+  `user_two_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `message_content` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `user_id` int(11) NOT NULL,
+  `display_name` varchar(30) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `age` int(11) NOT NULL,
+  `sex` varchar(6) NOT NULL,
+  `preferred_sex` varchar(6) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `bio` varchar(260) NOT NULL,
+  `picture` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` int(8) NOT NULL,
+  `banned` int(1) NOT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_interests`
+--
+
+CREATE TABLE `user_interests` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `interest_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `interactions`
+--
+ALTER TABLE `interactions`
+  ADD PRIMARY KEY (`interaction_id`),
+  ADD KEY `fk_interaction_user_id` (`user_id`),
+  ADD KEY `fk_interaction_target_user_id` (`target_user`),
+  ADD KEY `fk_interaction_type` (`interaction_type`);
+
+--
+-- Indexes for table `interaction_types`
+--
+ALTER TABLE `interaction_types`
+  ADD PRIMARY KEY (`interaction_type_id`);
+
+--
+-- Indexes for table `interests`
+--
+ALTER TABLE `interests`
+  ADD PRIMARY KEY (`interest_id`);
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`match_id`),
+  ADD KEY `fk_match_user_one_id` (`user_one_id`),
+  ADD KEY `fk_match_user_two_id` (`user_two_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `fk_message_sender_id` (`sender_id`),
+  ADD KEY `fk_message_receiver_id` (`receiver_id`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `fk_user_role` (`user_type`);
+
+--
+-- Indexes for table `user_interests`
+--
+ALTER TABLE `user_interests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_user_id` (`user_id`),
+  ADD KEY `fk_interest_id` (`interest_id`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `interactions`
+--
+ALTER TABLE `interactions`
+  MODIFY `interaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `interaction_types`
+--
+ALTER TABLE `interaction_types`
+  MODIFY `interaction_type_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `interests`
+--
+ALTER TABLE `interests`
+  MODIFY `interest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_interests`
+--
+ALTER TABLE `user_interests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `interactions`
+--
+ALTER TABLE `interactions`
+  ADD CONSTRAINT `fk_interaction_target_user_id` FOREIGN KEY (`target_user`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `fk_interaction_type` FOREIGN KEY (`interaction_type`) REFERENCES `interaction_types` (`interaction_type_id`),
+  ADD CONSTRAINT `fk_interaction_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `matches`
+--
+ALTER TABLE `matches`
+  ADD CONSTRAINT `fk_match_user_one_id` FOREIGN KEY (`user_one_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `fk_match_user_two_id` FOREIGN KEY (`user_two_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `fk_message_receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `fk_message_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk_user_role` FOREIGN KEY (`user_type`) REFERENCES `user_roles` (`role_id`);
+
+--
+-- Constraints for table `user_interests`
+--
+ALTER TABLE `user_interests`
+  ADD CONSTRAINT `fk_interest_id` FOREIGN KEY (`interest_id`) REFERENCES `interests` (`interest_id`),
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
