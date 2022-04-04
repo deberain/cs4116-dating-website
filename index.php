7782286bd5898664796867615722213f27c39dbc
Jonathan Falvey <JohnnyFalvey01@hotmail.com>
2022-04-04 22:54:26 +0100

    Changed layout of Index.php

--- index.php
+++ index.php
@@ -1,67 +1,66 @@
 <?php
-    session_start();
-    //If Already Logged IN
-    if(isset($_SESSION['LoggedIn'])) {
-        header('Location: home.php');
-        exit();
+session_start();
+//If Already Logged IN
+if (isset($_SESSION['LoggedIn'])) {
+    header('Location: home.php');
+    exit();
+}
+if (isset($_POST['login'])) {
+    include('config/connection.php');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    //to prevent from mysqli injection  
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+    $username = mysqli_real_escape_string($con, $username);
+    $password = mysqli_real_escape_string($con, $password);
+
+    $encryptedPassword = md5($password);
+
+    $sql = "select * from `users` where username = '$username' and password = '$encryptedPassword'";
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $userId = $row['user_id'];
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+        $_SESSION['LoggedIn'] = '1';
+        $_SESSION['user'] = $username;
+        $_SESSION['user_id'] = $userId;
+        $_SESSION['DOB'] = $row['date_of_birth'];
+
+        $sql = "select * from `profiles` where user_id = '$userId'";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        $_SESSION['display_name'] = $row['display_name'];
+        $_SESSION['location'] = $row['location'];
+        $_SESSION['sex'] = $row['sex'];
+        $_SESSION['pref'] = $row['preferred_sex'];
+        $_SESSION['bio'] = $row['bio'];
+        $_SESSION['photo'] = $row['picture'];
+
+        $sql = "select interest_id from `user_interests` where user_id = '$userId'";
+        $result = mysqli_query($con, $sql);
+        $data = array();
+
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row['interest_id'];
+        }
+
+        $_SESSION['interests'] = $data;
+
+        exit("Login Successful");
+    } else {
+        exit("Login Failed.");
     }
-    if(isset($_POST['login'])) {
-        include('config/connection.php');  
-        $username = $_POST['username'];  
-        $password = $_POST['password'];
-        
-            
-            //to prevent from mysqli injection  
-            $username = stripcslashes($username);  
-            $password = stripcslashes($password);  
-            $username = mysqli_real_escape_string($con, $username);  
-            $password = mysqli_real_escape_string($con, $password);  
-    
-            $encryptedPassword = md5($password);  
-            
-            $sql = "select * from `users` where username = '$username' and password = '$encryptedPassword'";  
-            $result = mysqli_query($con, $sql);  
-            $row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-            $userId = $row['user_id']; 
-            $count = mysqli_num_rows($result);  
-                
-            if($count == 1){
-                $_SESSION['LoggedIn'] = '1';
-                $_SESSION['user'] = $username;
-                $_SESSION['user_id'] = $userId;
-                $_SESSION['DOB'] = $row['date_of_birth'];
-
-                $sql = "select * from `profiles` where user_id = '$userId'";  
-                $result = mysqli_query($con, $sql);  
-                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-
-                $_SESSION['display_name'] = $row['display_name'];
-                $_SESSION['location'] = $row['location'];
-                $_SESSION['sex'] = $row['sex'];
-                $_SESSION['pref'] = $row['preferred_sex'];
-                $_SESSION['bio'] = $row['bio'];
-                $_SESSION['photo'] = $row['picture'];
-
-                $sql = "select interest_id from `user_interests` where user_id = '$userId'";  
-                $result = mysqli_query($con, $sql);
-                $data = array();
-
-                while ($row = mysqli_fetch_assoc($result))
-                {
-                    $data[] = $row['interest_id'];
-                }
-
-                $_SESSION['interests'] = $data;
-                  
-                exit("Login Successful");  
-            }  
-            else{  
-                exit("Login Failed.");  
-            }  
-    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -77,47 +76,52 @@
     <script defer src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
-<body>
-    <section class="welcome text-center">
-      <h1>Welcome to Cupid.com</h1>
-      <img defer class="rounded" src="assets/Pink_Heart.png" alt="sexy man" width="275" height="250"/>
-      <p />
-      <h3>Looking for Love? You came to the right place!</h3>
 
-      <div class="welcome-wave">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-        </svg>
-      </div>
-    </section>
-    <div id="login">
-        <div class="login-form">
-            <p>Login or register from here to access.</p>
-            <form name="loginForm" autocomplete="on">
-                <div class="form-group">
-                    <label>User Name</label>
-                    <input type="text" class="form-control" placeholder="User Name"  id="InputUsername" name="username">
-                </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" class="form-control" placeholder="Password" id="InputPassword" name="password">
-                    <input type="checkbox" onclick="showPassword()"> Show Password
-                </div>
-                <p id="FailedLogin"></p>
-                <button type="button" class="btn btn-purple" id="Login">Login</button>
-                <button type="button" onclick="document.location.href='register.html';"  class="btn btn-secondary">Register</button>
-            </form>
+<body>
+    <div class="welcome-container">
+        <div class="welcome-text text-center">
+            <h1>Welcome to Cupid.com</h1>
+            <img defer class="rounded" src="assets/Pink_Heart.png" alt="pink heart" width="275" height="250" />
+            <p>
+            <h3>Looking for Love? You came to the right place!</h3>
+        </div>
+
+        <div id="login">
+            <div class="login-form">
+                <p>Login or register from here to access.</p>
+                <form name="loginForm" autocomplete="on">
+                    <div class="form-group">
+                        <label>User Name</label>
+                        <input type="text" class="form-control" placeholder="User Name" id="InputUsername" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Password" id="InputPassword" name="password">
+                        <input type="checkbox" onclick="showPassword()"> Show Password
+                    </div>
+                    <p id="FailedLogin"></p>
+                    <button type="button" class="btn btn-purple" id="Login">Login</button>
+                    <button type="button" onclick="document.location.href='register.html';" class="btn btn-secondary">Register</button>
+                </form>
+            </div>
         </div>
     </div>
+    <section class="wv">
+        <div class="welcome-wave">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+            </svg>
+        </div>
+    </section>
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             console.log('Page Ready');
-            $("#Login").on('click', function () {
+            $("#Login").on('click', function() {
                 var username = $("#InputUsername").val();
                 var password = $("#InputPassword").val();
 
 
-                if(username == "" ||  password == "") {
+                if (username == "" || password == "") {
                     $("#FailedLogin").html("<font color='red'>Please enter both your Username and Password</font>");
                 } else {
                     $.ajax({
@@ -132,19 +136,18 @@
                     }).done(function(res) {
                         var result = String(res).trim();
                         console.log(result);
-                        if(result == "Login Failed.")
-                        {
+                        if (result == "Login Failed.") {
                             $("#FailedLogin").html("<font color='red'>Invalid Username or Password</font>");
                             return false;
                         } else {
-                            document.location.href="home.php";
+                            document.location.href = "home.php";
                         }
-                        
+
                     });
                 }
             });
         });
-        
+
         function showPassword() {
             var pWord = document.getElementById("InputPassword");
             if (pWord.type === "password") {
@@ -153,6 +156,7 @@
                 pWord.type = "password";
             }
         }
-    </script>  
+    </script>
 </body>
+
 </html>
