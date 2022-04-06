fb609c3609763ef9b4822433c6c5c703d6a505b7
Jonathan Falvey <JohnnyFalvey01@hotmail.com>
2022-04-06 19:54:35 +0100

    like_user returns more useful diagnostic information

--- config/like_user.php
+++ config/like_user.php
@@ -9,23 +9,27 @@ if (!empty($_POST['target_id'])) {
     //include database configuration file
     include_once 'connection.php';
     //insert form data in the database
-    $sql = "select * from `interactions` where user_id = '$user_id' and target_user = '$target_id' and interaction_type = '1'";  
-    $result = mysqli_query($con, $sql);  
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-    $count = mysqli_num_rows($result);  
-        
-    if($count == 1){  
-        exit("Already Liked User");  
-    }  
+    $sql = "select * from `interactions` where user_id = '$user_id' and target_user = '$target_id' and interaction_type = '1'";
+    $result = mysqli_query($con, $sql);
 
-    $sql = "INSERT INTO `interactions` (`interaction_id`, `user_id`, `target_user`, `interaction_type`, `date`) VALUES (NULL, '$user_id', '$target_id', '1', date('Y-m-d H:i:s'))";
+    if ($result) {
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
 
-    if ($con->query($sql) === TRUE) {
-        exit('Success!');
+        if ($count == 1) {
+            exit("Already Liked User");
+        }
+
+        $sql = "INSERT INTO `interactions` (`interaction_id`, `user_id`, `target_user`, `interaction_type`, `date`) VALUES (NULL, '$user_id', '$target_id', '1', date('Y-m-d H:i:s'))";
+
+        if ($con->query($sql) === TRUE) {
+            exit('Success!');
+        } else {
+            die(mysqli_error($con));
+        }
     } else {
-        die('Failure');
+        die(mysqli_error($con));
     }
 } else {
-    die("Invalid");
+    die("No target id in payload");
 }
-?>
